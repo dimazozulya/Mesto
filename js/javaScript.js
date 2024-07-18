cdcf262b9fdc7320011ce7f3bea50c71410fb359
Dima Zozulya <yolllohood@yadnex.ru>
2024-07-18 23:11:55 +0300

    commit Mesto Created

---
+++ js/javaScript.js
@@ -0,0 +1,45 @@
+
+let modal = document.getElementById('popup');
+let closeButton = document.querySelector('.closeBtn');
+let openButton = document.querySelector('.profile__name-btn');
+
+function openModal() {
+  modal.classList.add('popup_active');
+}
+function closeModal() {
+  modal.classList.remove('popup_active');
+}
+closeButton.addEventListener('click', closeModal);
+openButton.addEventListener('click', openModal);
+
+window.addEventListener('click', function(event) {
+  if (event.target === modal) {
+    closeModal();
+  }
+});
+
+
+
+let formElement = document.querySelector('#inputs');
+
+let nameInput = document.querySelector('.popup__input-name');
+let jobInput = document.querySelector('.popup__input-job');
+
+function handleFormSubmit(evt) {
+    evt.preventDefault(); 
+
+    let nameValue = nameInput.value;
+    let jobValue = jobInput.value;
+
+    let nameDisplay = document.querySelector('.profile__name-text');
+    let jobDisplay = document.querySelector('.profile__subtitle');
+
+    nameDisplay.textContent = nameValue;
+    jobDisplay.textContent = jobValue;
+}
+
+    let saveButton = document.querySelector('.popup__submit-button');
+
+formElement.addEventListener('submit', handleFormSubmit);
+saveButton.addEventListener('click', closeModal);
+
