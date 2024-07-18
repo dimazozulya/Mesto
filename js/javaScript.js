efffdf3e787e931edc11869662ef805cb19a8866
Timur Enakaev <timur.enakaev.2000@gmail.com>
2024-07-19 01:04:37 +0300

    zamechania dla demona

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,4 +1,5 @@
-
+// почему ты все переменные объявляешь через let, если они все у тебя неизменяемые?
+// по дефолту всегда надо объявлять через const
 let modal = document.getElementById('popup');
 let closeButton = document.querySelector('.closeBtn');
 let openButton = document.querySelector('.profile__name-btn');
@@ -17,7 +18,7 @@ window.addEventListener('click', function(event) {
     closeModal();
   }
 });
-
+//Можно выебнуться и так написать, но у тебя более читаемое, так что оставь своё решение, моё ты все равно скорее всего не понял ;) --- window.addEventListener('click', (e) => e.target === modal ? closeModal() : null);
 
 
 
