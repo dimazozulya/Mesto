6698a4199abca01c50392ca797697c55842fcf90
Dima Zozulya <yolllohood@yadnex.ru>
2024-11-16 17:14:05 +0300

    bug fixes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
+import FormValidator from './FormValidator.js';
+import { Card, CardForm } from './card.js';
+import { initialCards } from './constants.js';
 
 const modal = document.getElementById('popup-profile');
 const closeButton = document.querySelector('.popup__close-btn');
 const openButton = document.querySelector('.profile__name-btn');
+const modalCard = document.getElementById('popup-card');
+const closeButtonCard = document.querySelector('.popup__close-btn-card');
+const openButtonCard = document.querySelector('.profile__btn');
+const formElement = document.querySelector('#form');
+const nameInput = document.querySelector('.popup__input-name');
+const jobInput = document.querySelector('.popup__input-job');
+const saveButton = document.querySelector('.popup__submit-button');
 
 function openModal() {
   modal.classList.add('popup_active');
@@ -29,10 +39,6 @@ window.addEventListener('click', function(event) {
   };
 });
 
-const modalCard = document.getElementById('popup-card');
-const closeButtonCard = document.querySelector('.popup__close-btn-card');
-const openButtonCard = document.querySelector('.profile__btn');
-
 function openModalCard() {
   modalCard.classList.add('popup_active');
   window.addEventListener ('keydown', handleEscCloseCard);
@@ -55,10 +61,6 @@ window.addEventListener('click', function(event) {
     closeModalCard();
   }
 });
-const formElement = document.querySelector('#form');
-
-const nameInput = document.querySelector('.popup__input-name');
-const jobInput = document.querySelector('.popup__input-job');
 
 function handleFormSubmit(evt) {
     evt.preventDefault(); 
@@ -73,40 +75,9 @@ function handleFormSubmit(evt) {
     jobDisplay.textContent = jobValue;
 };
 
-const saveButton = document.querySelector('.popup__submit-button');
 
 formElement.addEventListener('submit', handleFormSubmit);
 saveButton.addEventListener('click', closeModal);
-
-  const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ];
-
-
-import { Card, CardForm } from './card.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initialCards.forEach((item) => {
@@ -116,13 +87,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const cardForm = new CardForm('#form-card', '#content', '#card', 'popup-card');
 });
 
-
-import FormValidator from './FormValidator.js';
-
 document.addEventListener('DOMContentLoaded', () => {
   const formProfile = new FormValidator('#form', '.popup__input', '.popup__submit-button', '.error-message');
   const formCard = new FormValidator('#form-card', '.popup__input', '.popup__submit-button-card', '.error-message');
   formProfile.enableValidation();
   formCard.enableValidation();
-  
 });
